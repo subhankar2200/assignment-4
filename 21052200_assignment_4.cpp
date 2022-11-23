8398c2d8e7a002888f37b2b332e8e57c97e23911
subhankar2200 <118849648+subhankar2200@users.noreply.github.com>
2022-11-23 12:47:58 +0530

    Update 21052200_assignment_4.cpp

--- 21052200_assignment_4.cpp
+++ 21052200_assignment_4.cpp
@@ -1,26 +1,112 @@
-#include <iostream>
+#include<iostream>
 #include <windows.h>
-#include <string.h>
+#include<string>
 using namespace std;
-class Shape
-{
-private:
-    int size;
 
-public:
-    void Move()
-    {
-        char c[50] = "Moving the Object to Given Coordinates ......";
-        gotoxy(50, 8);
-        for (int i = 0; i < 50; i++)
-        {
-            cout << c[i];
-            Sleep(200);
-        }
-    }
+class Books//Stores and displays original details of the book
+{
+	public:
+	int id, count;
+	double price;
+	string author, name;
+	void input()
+	{
+	cout<<"Enter the details of the book:"<<endl;
+	cout<<"ID: "<<endl;
+	cin>> id;
+	cout<<"NAME:"<<endl;
+	cin>>name;
+	cout<<"PRICE: "<< endl;
+	cin>> price;
+	cout<<"NAME OF AUTHOR: "<<endl;
+	cin>>author;
+	cout<<"THE QUANTITY: "<<endl;
+	cin>>count;
+	}
+	void display()
+	{
+	cout<<"ORIGINAL DETAILS OF THE BOOK:"<<endl;
+	cout<<"ID:"<< id<<"\n NAME:"<< name<< "\n ORIGINAL PRICE: "<<price<<"\n AUTHOR: "<<author<<"\n COUNT: "<<count<<endl;
+
+	}
+
+};
+class Supplier: public Books//Takes and updates details about books from potential suppliers
+{
+	public:
+	int change;
+	double new_price;
+	void check()
+	{
+	cout<<"Do you want to make any modifications to the price of the book you are selling?"<<endl;
+	cout<<"Enter 0 for no and 1 for yes"<<endl;
+	cin>>change;
+	switch(change)
+	{
+	case 0: cout<<"No changes are being made to the book's price and being supplied"<<endl;
+	break;
+	case 1: cout<<"What is the price for which you wish to sell the book?"<<endl;
+	cout<<"REMEMBER: WE DON'T BUY BOOKS AT A COST WHICH EXCEEDS THEIR MRP BY 100 RUPPEES"<<endl;
+	cin>>new_price;
+	if(new_price>price)
+	{
+	if((new_price-price)>100)
+	{
+	cout<<"SORRY. WE DO NOT ACCEPT THE PROPOSED PRICE AND ARE NOT BUYING THE BOOK"<<endl;
+	count=0;
+	}
+	else
+	price=new_price;
+	}
+	else
+	price=new_price;
+	break;
+	default: cout<<"Wrong option chosen. We are considering there are no changes in the pricing and the book is considered supplied at its original price "<<endl;
+	}
+
+
+	}
+	void show(){
+	cout<< "UPDATED PRICE OF THE BOOK NOW: "<<new_price;
+	}
+	};
+	class Buyer//Customer buys the book
+	{
+	public:
+	int bID;
+	void getId()
+	{
+	cout<<"Enter the id of the book you want to buy:"<<endl;
+	cin>>bID;
+	}
+	};
+	class Rent//Person rents the book
+	{
+	public:
+	int rID;
+	void getrId()
+	{
+	cout<<"Enter the id of the book you want to rent:"<<endl;
+	cin>>rID;
+	}};
+	class ReturnBook//Person returns previously rented book
+	{
+	public:
+	int d;
+	void ret()
+	{
+	cout<<"How many days later are you returning the book?"<<endl;
+	cin>>d;
+	if(d>15)
+	cout<<"You have to pay a fine of 100 ruppees"<<endl;
+	else
+	cout<<"Thank you for returning the book on time"<<endl;	
+	}
+
+	};
     void Intro()
     {
-        char intro[200] = "\t\t\t\tOOP Theory Project\n\t\t\tSubmitted By - Roushan Singh\n\t\t\tSubmitted To-" ;
+    char intro[200] = "\t\t\t\tOOP Theory Project\n\t\t\tSubmitted By - Subhankar Ghosh\n\t\t\tRoll No : 21052200 " ;
         for ( int i = 0; i < strlen(intro); i++)
         {
             cout << intro[i];
@@ -28,377 +114,105 @@ public:
         }
         Sleep(2000);
     }
-    void gotoxy(int x, int y)
-    {
-        COORD coord;
-        coord.X = x;
-        coord.Y = y;
-        SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
-    }
-};
-class Complex : public Shape
-{
-private:
-    int x = 0, y = 0, size = 15;
+	int main()
+	{
+        Intro();
+	cout<<"Enter an option from below:"<<endl;
+	cout<<"1: SUPPLY A BOOK"<<endl;
+	cout<<"2: BUY A BOOK"<<endl;
+	cout<<"3:RENT A BOOK"<<endl;
+	cout<<"4: RETURN BOOK(PREVIOUSLY RENTED)"<<endl;
+	cout<<"5. EXIT"<<endl;
+	int op;
+	cin>>op;
+	Books ob[150];
+	Supplier s;
+	Buyer b;
+	Rent r;
+	ReturnBook R;
+	int i=0;
+	do
+	{
+	switch(op)
+	{
+	case 1:ob[i].input();
+	ob[i].display();
+	s.check();
+	s.show();
+	i++;
+	break;
+	case 2:
+	{
+	b.getId();
+	int j=i;
+	while(j!=-1)
+	{
+	if( ob[i].id==b.bID)
+	{
+	if(ob[j].count!=0)
+	{
+	cout<<"SOLD TO YOU"<<endl;
+	ob[j].count--;
+	ob[j].display();
+	}
+	else
+	cout<<"Sorry not in stock"<<endl;
 
-public:
-    void Choice()
-    {
-        int a = 0, b = 0;
-        char c;
-        cout << "Do you want to move shape (Yes-Y and No-N): ";
-        cin >> c;
-        if (c == 'y' || c == 'Y')
-        {
-            cout << "Enter the next coordinate to move : ";
-            cin >> a >> b;
-            system("CLS");
-            FastHeart();
-            x = a;
-            y = b;
-            Move();
-            Sleep(3000);
-            system("CLS");
-            SlowHeart();
-        }
-        else
-            SlowHeart();
-        system("pause");
-    }
-    void SlowHeart()
-    {
-        gotoxy(x, y++);
-        for (int i = size / 2; i < size; i += 2)
-        {
-            for (int j = 1; j < size - i; j += 2)
-            {
-                Sleep(5);
+	}
+	else
+	cout<<"Sorry we do not have that book"<<endl;
+	j--;
+	}
+	}
 
-                cout << " ";
-            }
-            for (int j = 1; j < i + 1; j++)
-            {
-                Sleep(5);
+	break;
 
-                cout << "*";
-            }
+	case 3: {
 
-            for (int j = 1; j < size - i + 1; j++)
-            {
-                Sleep(5);
+	r.getrId();
+	int j=i;
+	while(j!=-1)
+	{
+	if( ob[j].id==r.rID)
+	{
+	if(ob[j].count!=0)
+	{
+	cout<<"RENTED TO YOU"<<endl;
+	ob[j].count--;
+	ob[j].display();
+	}
+	else
+	cout<<"Sorry not in stock"<<endl;
 
-                cout << " ";
-            }
-            for (int j = 1; j < i + 1; j++)
-            {
+	}
+	else
+	cout<<"Sorry we do not have that book"<<endl;
+	j--;
+	}}
+	break;
+	case 4:{
+	cout<<"STATE THE BOOK ID OF THE BOOK YOU ARE RETURNING"<<endl;
+	int b_id;
+	cin>>b_id;
+	int j=i;
+	while(j!=-1)
+	{
+	if(ob[j].id==b_id)
+	{
 
-                Sleep(5);
+	ob[j].count++;
+	cout<<"Book returned succesfully"<<endl;
+	ob[j].display();
+	}
+	else
+	cout<<"Wrong id"<<endl;
+	j--;	
+	}
+	R.ret();}
+	break;
+	default:cout<<"Wrong option. Try Again"<<endl;
+	}
 
-                cout << "*";
-            }
-            gotoxy(x, y++);
-        }
-        for (int i = size; i > 0; i--)
-        {
-            for (int j = 0; j < size - i; j++)
-            {
-                Sleep(5);
-
-                cout << " ";
-            }
-            for (int j = 1; j < i * 2; j++)
-            {
-                Sleep(5);
-
-                cout << "*";
-            }
-            gotoxy(x, y++);
-        }
-    }
-    void FastHeart()
-    {
-        gotoxy(x, y++);
-        for (int i = size / 2; i < size; i += 2)
-        {
-            for (int j = 1; j < size - i; j += 2)
-            {
-                cout << " ";
-            }
-            for (int j = 1; j < i + 1; j++)
-            {
-                cout << "*";
-            }
-
-            for (int j = 1; j < size - i + 1; j++)
-            {
-                cout << " ";
-            }
-            for (int j = 1; j < i + 1; j++)
-            {
-                cout << "*";
-            }
-            gotoxy(x, y++);
-        }
-        for (int i = size; i > 0; i--)
-        {
-            for (int j = 0; j < size - i; j++)
-            {
-                cout << " ";
-            }
-            for (int j = 1; j < i * 2; j++)
-            {
-                cout << "*";
-            }
-            gotoxy(x, y++);
-        }
-    }
-};
-class SimpleShape : public Shape
-{
-public:
-    int x = 0, y = 0, n = 15;
-    void SetData()
-    {
-        cout << "Enter the Size for Simple Shape: ";
-        cin >> this->n;
-    }
-};
-class Box : public SimpleShape
-{
-private:
-public:
-    void Choice()
-    {
-        int a = 0, b = 0;
-        char c;
-        cout << "Do you want to move shape (Yes-Y and No-N): ";
-        cin >> c;
-        if (c == 'y' || c == 'Y')
-        {
-            cout << "Enter the next coordinate to move : ";
-            cin >> a >> b;
-            system("CLS");
-            FastBox();
-            x = a;
-            y = b;
-            Move();
-            Sleep(3000);
-            system("CLS");
-            SlowBox();
-        }
-        else
-            SlowBox();
-        system("pause");
-    }
-    void SlowBox()
-    {
-        int i, j, k;
-        gotoxy(x, y++);
-        cout << " ";
-        for (i = 0; i < this->n; i++)
-        {
-            if (i == 0)
-            {
-                for (j = 2; j < this->n; j++)
-                {
-                    cout << "_";
-                    Sleep(150);
-                }
-            }
-            else if (i == (this->n - 1))
-            {
-                for (j = 0; j < this->n; j++)
-                {
-                    Sleep(150);
-                    if (j == 0 || j == (this->n - 1))
-                    {
-                        cout << "|";
-                    }
-                    else
-                        cout << "_";
-                }
-            }
-            else
-            {
-                for (k = 0; k < this->n; k++)
-                {
-                    if (k == 0 || k == (this->n - 1))
-                    {
-                        Sleep(150);
-
-                        cout << "|";
-                    }
-                    else
-                        cout << " ";
-                }
-            }
-            gotoxy(x, y++);
-        }
-    }
-    void FastBox()
-    {
-        int i, j, k;
-        gotoxy(x, y++);
-        cout << " ";
-        for (i = 0; i < this->n; i++)
-        {
-            if (i == 0)
-            {
-                for (j = 2; j < this->n; j++)
-                {
-                    cout << "_";
-                }
-            }
-            else if (i == (this->n - 1))
-            {
-                for (j = 0; j < this->n; j++)
-                {
-                    if (j == 0 || j == (this->n - 1))
-                    {
-                        cout << "|";
-                    }
-                    else
-                        cout << "_";
-                }
-            }
-            else
-            {
-                for (k = 0; k < this->n; k++)
-                {
-                    if (k == 0 || k == (this->n - 1))
-                    {
-
-                        cout << "|";
-                    }
-                    else
-                        cout << " ";
-                }
-            }
-            gotoxy(x, y++);
-        }
-    }
-};
-class Tent : public SimpleShape
-{
-private:
-    int x = 0, y = 0, n = 15;
-
-public:
-    void Choice()
-    {
-        int a = 0, b = 0;
-        char c;
-        cout << "Do you want to move shape (Yes-Y and No-N): ";
-        cin >> c;
-        if (c == 'y' || c == 'Y')
-        {
-            cout << "Enter the next coordinate to move : ";
-            cin >> a >> b;
-            system("CLS");
-            FastTent();
-            x = a;
-            y = b;
-            Move();
-            Sleep(3000);
-            system("CLS");
-            Slowtent();
-        }
-        else
-            Slowtent();
-        system("pause");
-    }
-    void Slowtent()
-    {
-        int i, j, k;
-        gotoxy(x, y++);
-        for (i = 0; i < n; i++)
-        {
-            Sleep(150);
-            for (j = 0; j <= (n - i); j++)
-            {
-                if (i <= n - 1)
-                    cout << " ";
-            }
-            cout << "/";
-            for (int a = 0; a <= i; a++)
-            {
-                if (a == i)
-                    cout << "\\";
-                else
-                {
-                    if (i == (n - 1))
-                    {
-                        Sleep(100);
-                        cout << "__";
-                    }
-                    else
-                        cout << "  ";
-                }
-            }
-            gotoxy(x, y++);
-        }
-    }
-    void FastTent()
-    {
-        int i, j, k;
-        for (i = 0; i < n; i++)
-        {
-            for (j = 0; j <= (n - i); j++)
-            {
-                if (i <= n - 1)
-                    cout << " ";
-            }
-            cout << "/";
-            for (int a = 0; a <= i; a++)
-            {
-                if (a == i)
-                    cout << "\\";
-                else
-                {
-                    if (i == (n - 1))
-                    {
-                        cout << "__";
-                    }
-                    else
-                        cout << "  ";
-                }
-            }
-            cout << endl;
-        }
-    }
-};
-int main()
-{
-    system("CLS");
-    Shape S;
-    Complex C;
-    SimpleShape Si;
-    Box B;
-    Tent T;
-    int c = 0;
-    S.Intro();
-    while (c != 4)
-    {
-        system("CLS");
-        cout << "Shape Selection : \n1-Complex Shape\n2-Box\n3-Tent\n4-Exit\nYour Choice: ";
-        cin >> c;
-        switch (c)
-        {
-        case 1:
-            C.Choice();
-            break;
-        case 2:
-            B.Choice();
-            break;
-        case 3:
-            T.Choice();
-            break;
-        case 4:
-            return 0;
-        default:
-            cout << "\nWrong Input !!\nRestarting the Program !!\n";
-            system("pause");
-            main();
-        }
-    }
-}
+	}while(op!=5);
+	return 0;
+	}
